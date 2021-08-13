14239251d94af39da71b6d8889e8dcb239dfaf86
Bed1234 <76521252+Bed1234@users.noreply.github.com>
2021-08-13 09:42:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//setting the property for the webcam 
+Webcam.set({
+    width:310,
+    height:300,
+    image_format:'png',
+    png_quality:90,
+
+
+    constraints:{
+     facingMode:"environment"
+    }
+});
+
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
